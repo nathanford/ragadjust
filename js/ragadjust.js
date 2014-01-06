0a9da52fb4bbbad5b12b741583506473304b15c9
Nathan Ford <nathan@markboultondesign.com>
2014-01-06 13:06:54 +0000

    Fixed prepositions regex to not remove leading spaces and added prepositions to README

--- js/ragadjust.js
+++ js/ragadjust.js
@@ -5,7 +5,7 @@ ragadjust = function (s, method) {
 		var eles = document.querySelectorAll(s),
 				elescount = eles.length,
 				
-				preps = /(\s|^|>)((aboard|about|above|across|after|against|along|amid|among|anti|around|before|behind|below|beneath|beside|besides|between|beyond|concerning|considering|despite|down|during|except|excepting|excluding|following|from|inside|into|like|minus|near|onto|opposite|outside|over|past|plus|regarding|round|save|since|than|that|this|through|toward|towards|under|underneath|unlike|until|upon|versus|with|within|without)?\s)+/gi,
+				preps = /(\s|^|>)((aboard|about|above|across|after|against|along|amid|among|anti|around|before|behind|below|beneath|beside|besides|between|beyond|concerning|considering|despite|down|during|except|excepting|excluding|following|from|inside|into|like|minus|near|onto|opposite|outside|over|past|plus|regarding|round|save|since|than|that|this|through|toward|towards|under|underneath|unlike|until|upon|versus|with|within|without)\s)+/gi,
 				
 				smallwords = /(\s|^)(([a-zA-Z-_(]{1,2}('|’)*[a-zA-Z-_,;]{0,1}?\s)+)/gi, // words with 3 or less characters
 				
